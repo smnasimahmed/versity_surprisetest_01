a7277b8badb75feb6bb0a7fe7c9bceeced81895f
S M Nasim Ahmed <111959129+smnasimahmed@users.noreply.github.com>
2023-11-27 04:36:58 +0600

    removedcustom_icon.dart

--- lib/custom_icon.dart
+++ lib/custom_icon.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class Custom_nev_icon extends StatelessWidget {
-  const Custom_nev_icon({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 5,
-        width: 5,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(50)),
-        color: Colors.black,
-        child: Icon(Icons.navigate_next));
-  }
-}
